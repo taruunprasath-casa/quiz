7f66aed4cd7505ec70b8cad4a677700aeece1aa9
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-09-18 18:17:04 +0530

    Taruun | router provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import Dashboard from './pages/Dashboard'
+import { RouterProvider } from "react-router-dom"
+import router from "./router"
+
 
 const App = () => {
   return (
     <>
-      <Dashboard />
+      <RouterProvider router={router} />
     </>
   )
 }
